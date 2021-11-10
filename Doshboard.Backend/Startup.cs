8c5aae6d885f1174d655d1da7f785de4581fc67e
Melefo <melefo@melefo.xyz>
2021-11-10 15:50:45 +0100

    Oldschool main+startup

---
+++ Doshboard.Backend/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Doshboard.Backend
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Doshboard", Version = "v1" });
+            });
+            services.AddEndpointsApiExplorer();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
