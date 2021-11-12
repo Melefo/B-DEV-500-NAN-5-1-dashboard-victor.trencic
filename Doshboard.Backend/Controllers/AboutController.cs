364d4af20285d89e765c0f010053ed92aa1cbf0c
mllusar <mathis.llusar@epitech.eu>
2021-11-12 14:17:34 +0100

    Adding basic About Controller and Model

---
+++ Doshboard.Backend/Controllers/AboutController.cs
@@ -0,0 +1,29 @@
+﻿using Doshboard.Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Doshboard.Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AboutController : ControllerBase
+    {
+        [HttpGet]
+        public AboutModel Get()
+        {
+            string ClientIp = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            return new AboutModel
+            {
+                Client = new()
+                {
+                    Host = ClientIp,
+                },
+                Server = new()
+                {
+                    CurrentTime = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds
+                }
+            };
+
+
+        }
+    }
+}
---
+++ Doshboard.Backend/Models/AboutModel.cs
@@ -0,0 +1,42 @@
+﻿namespace Doshboard.Backend.Models
+{
+    public class Client
+    {
+        public string? Host { get;  set; }
+    }
+    public enum ParamType
+    {
+        String = 0,
+        Interger = 1
+    }
+
+    public class Param
+    {
+        public string Name { get; set; }
+        public ParamType Type { get; set; }
+    }
+
+    public class Widget
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public List<Param> Params { get; set; } = new();
+    }
+
+    public class Service
+    {
+        public string? Name { get; set; }
+        public List<Widget> Widgets { get; set; }
+    }
+
+    public class ServerModel
+    {
+        public long CurrentTime { get; set; }
+        public List<Service> Services { get; set; }
+    }
+    public class AboutModel
+    {
+        public Client Client { get; set; } = new();
+        public ServerModel Server { get; set; } = new();
+    }
+}
