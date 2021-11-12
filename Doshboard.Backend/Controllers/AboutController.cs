71c201301d94b3f128c3d84adf60d1b8e575cb01
Melefo <melefo@melefo.xyz>
2021-11-12 14:40:44 +0100

    Better model

--- Doshboard.Backend/Controllers/AboutController.cs
+++ Doshboard.Backend/Controllers/AboutController.cs
@@ -8,22 +8,11 @@ namespace Doshboard.Backend.Controllers
     public class AboutController : ControllerBase
     {
         [HttpGet]
-        public AboutModel Get()
+        public About Get()
         {
-            string ClientIp = Request.HttpContext.Connection.RemoteIpAddress.ToString();
-            return new AboutModel
-            {
-                Client = new()
-                {
-                    Host = ClientIp,
-                },
-                Server = new()
-                {
-                    CurrentTime = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds
-                }
-            };
+            string clientIp = Request.HttpContext.Connection.RemoteIpAddress?.ToString() ?? string.Empty;
 
-
+            return new(clientIp);
         }
     }
 }
---
+++ Doshboard.Backend/Models/About.cs
@@ -0,0 +1,66 @@
+﻿namespace Doshboard.Backend.Models
+{
+    public class Client
+    {
+        public Client(string host) =>
+            Host = host;
+
+        public string Host { get; set; }
+    }
+    public enum ParamType
+    {
+        String = 0,
+        Interger = 1
+    }
+
+    public class Param
+    {
+        public Param(string name, ParamType type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public string Name { get; set; }
+        public ParamType Type { get; set; }
+    }
+
+    public class Widget
+    {
+        public Widget(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Param> Params { get; set; } = new();
+    }
+
+    public class Service
+    {
+        public Service(string name) =>
+            Name = name;
+
+        public string Name { get; set; }
+        public List<Widget> Widgets { get; set; } = new();
+    }
+
+    public class Server
+    {
+        public Server() =>
+            CurrentTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+        public long CurrentTime { get; set; }
+        public List<Service> Services { get; set; } = new();
+    }
+    public class About
+    {
+        public About(string host) =>
+            Client = new(host);
+
+        public Client Client { get; set; }
+        public Server Server { get; set; } = new();
+    }
+}
