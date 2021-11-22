afe1c8f1fc1a3706d763019d5e32ec7ac0444ac9
mllusar <mathis.llusar@epitech.eu>
2021-11-22 14:42:20 +0100

    Adding Email in User table

--- Doshboard.Backend/Entities/User.cs
+++ Doshboard.Backend/Entities/User.cs
@@ -8,6 +8,7 @@ namespace Doshboard.Backend.Entities
         [BsonId]
         public ObjectId Id { get; private init; } = ObjectId.GenerateNewId();
         public string Username { get; set; }
+        public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Password { get; set; }
