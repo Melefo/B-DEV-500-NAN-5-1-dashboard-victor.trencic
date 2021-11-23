be1e03b443749d6199b73e8939ca288720dda81d
mllusar <mathis.llusar@epitech.eu>
2021-11-23 13:46:46 +0100

    Upating User Services : adding authentication with Jwt token

--- Doshboard.Backend/Services/UserService.cs
+++ Doshboard.Backend/Services/UserService.cs
@@ -1,13 +1,20 @@
 ﻿using Doshboard.Backend.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
 
 namespace Doshboard.Backend.Services
 {
     public class UserService
     {
         private Mongo _db;
-        public UserService(Mongo db)
+        private readonly string key;
+        public UserService(Mongo db, IConfiguration config)
         {
             _db = db;
+            key = config["JwtKey"];
         }
 
         public List<User> GetUsers() => _db.GetUsers();
@@ -16,6 +23,36 @@ namespace Doshboard.Backend.Services
 
         public void Create(User user) => _db.CreateUser(user);
 
-        public void Delete(User user) => _db.DeleteUser(user);
+        public void Delete(string id) => _db.DeleteUser(id);
+
+        public (string, User) Authenticate(string email, string username, string password)
+        {
+            var user = _db.GetFilteredUser(x => (x.Email == email || x.Username == username) && x.Password == password);
+
+            if (user == null)
+                return (null, null);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var tokenKey = Encoding.UTF8.GetBytes(key);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Email, email),
+                }),
+
+                Expires = DateTime.UtcNow.AddHours(1),
+
+                SigningCredentials = new SigningCredentials (
+                        new SymmetricSecurityKey(tokenKey),
+                        SecurityAlgorithms.HmacSha256
+            )};
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);     
+
+            return (tokenHandler.WriteToken(token), user);
+        }
     }
 }
