c49189df5393f97a1d43d288f3e2a397e7351051
mllusar <mathis.llusar@epitech.eu>
2021-11-26 16:53:32 +0100

    Adding ROoee for user

--- Doshboard.Backend/Entities/User.cs
+++ Doshboard.Backend/Entities/User.cs
@@ -36,6 +36,10 @@ namespace Doshboard.Backend.Entities
         /// </summary>
         public string Password { get; set; }
         /// <summary>
+        /// User Role
+        /// </summary>
+        public string Role { get; set; } = "User";
+        /// <summary>
         /// User constructor
         /// </summary>
         /// <param name="username">Username</param>
