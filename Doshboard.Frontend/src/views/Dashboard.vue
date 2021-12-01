6aa8aedc8ee80a6a1e3ef0b64fbd30307b3bc0e3
Melefo <melefo@melefo.xyz>
2021-12-01 11:18:17 +0100

    Better grid

--- Doshboard.Frontend/src/views/Dashboard.vue
+++ Doshboard.Frontend/src/views/Dashboard.vue
@@ -11,7 +11,7 @@
             </div>
             <div id="right">
                 <vuescroll>
-                    <grid-layout :layout.sync="layout" :col-num="12" :row-height="30" :is-draggable="true" :is-resizable="false" :is-mirrored="false" :vertical-compact="true" :margin="[10, 10]" :use-css-transforms="true">
+                    <grid-layout :layout.sync="layout" :col-num="6" :row-height="80" :is-draggable="true" :is-resizable="false" :is-mirrored="false" :vertical-compact="true" :margin="[20, 20]" :use-css-transforms="true">
                         <grid-item v-for="(item, index) in layout" :x="item.x" :y="item.y" :w="item.w" :h="item.h" :i="item.i" :key="item.i">
                             <Weather v-if="item.type == 0" :id=index />
                         </grid-item>
@@ -99,17 +99,17 @@ i {
         },
         created: async function() {
             const data = await this.get();
-            const test = data.map((item) => {
+            this.layout = data.map((item) => {
+                console.log(item);
                 return {
-                    x:item.x,
-                    y:item.y,
-                    w:item.width,
-                    h:item.height,
-                    i:item.id,
+                    x: item.x,
+                    y: item.y,
+                    w: item.width,
+                    h: item.height,
+                    i: item.id,
                     type: item.type
                 }
             })
-            this.layout = test
         }
     })
 </script>
