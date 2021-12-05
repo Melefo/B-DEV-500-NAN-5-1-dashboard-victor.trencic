adf08b70dfe9549ff43b4a119f527a8629febe81
Melefo <melefo@melefo.xyz>
2021-12-05 19:27:22 +0100

    Fixing key

--- Doshboard.Backend/Utilities/ClientAPI.cs
+++ Doshboard.Backend/Utilities/ClientAPI.cs
@@ -6,25 +6,33 @@ namespace Doshboard.Backend.Utilities
     {
         private static HttpClient _client = new();
 
-        public static async Task<T?> GetAsync<T>(string url)
+        public static async Task<T?> GetAsync<T>(string url, KeyValuePair<string, string> header = default)
         {
+            if (!header.Equals(default))
+                _client.DefaultRequestHeaders.Add(header.Key, header.Value);
             var response = await _client.GetAsync(url);
 
             if (!response.IsSuccessStatusCode)
                 return default;
             if (response.StatusCode == HttpStatusCode.NoContent)
                 return default;
+            if (!header.Equals(default))
+                _client.DefaultRequestHeaders.Remove(header.Key);
             return await response.Content.ReadFromJsonAsync<T>();
         }
 
-        public static async Task<T?> PostAsync<T>(string url)
+        public static async Task<T?> PostAsync<T>(string url, KeyValuePair<string, string> header = default)
         {
+            if (!header.Equals(default))
+                _client.DefaultRequestHeaders.Add(header.Key, header.Value);
             var response = await _client.PostAsync(url, null);
 
             if (!response.IsSuccessStatusCode)
                 return default;
             if (response.StatusCode == HttpStatusCode.NoContent)
                 return default;
+            if (!header.Equals(default))
+                _client.DefaultRequestHeaders.Remove(header.Key);
             return await response.Content.ReadFromJsonAsync<T>();
         }
     }
